0f54854b86db7d0ba550475bee351d5485d6e694
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2023-03-09 21:14:58 +0000

    Update Exerc2_4.py

--- Exerc2_4.py
+++ Exerc2_4.py
@@ -1,3 +1,5 @@
+#Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número inteiro entre 
+#1 a 10. O utilizador deve informar de qual numero ele deseja ver a tabuada
 numero = int(input("Digite um numero de 1 a 10: "))
 for i in range(1, 11):
     print(f"{numero} X {i} = {numero * i}")
