d1c58b0a8f6cca1c3b67a88297af41ceb7009ec7
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2023-03-09 21:18:03 +0000

    Update Exerc2_7.py

--- Exerc2_7.py
+++ Exerc2_7.py
@@ -1,4 +1,4 @@
- 
+#Faça um programa que calcule o fatorial de um número inteiro fornecido pelo utilizador
 print("Cálculo do fatorial de um número")
 n = int(input("Digite um número inteiro não-negativo: "))
 def fatorial(n):
