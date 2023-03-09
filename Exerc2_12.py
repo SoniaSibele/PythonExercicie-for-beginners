3da31d24d0089396a1331ae93b9acb8ea0267963
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2023-03-09 21:31:03 +0000

    Update Exerc2_12.py

--- Exerc2_12.py
+++ Exerc2_12.py
@@ -1,24 +1,11 @@
-#s= str(input("Digite uma palavra: "))
-#def isPalindrome(s):
-    #return s == s[::-1]
-  
-#ans = isPalindrome(s)
-  
-#if ans:
-    #print("Yes")
-#else:
-    #print("No")
-
+# Escreva um programa que lê uma string e determina se a mesma é um palíndromo.
 def is_palindrome(string):
-  # remove any spaces and make all characters lowercase
+ 
   string = string.replace(" ", "").lower()
-
-  # reverse the string
   reversed_string = string[::-1]
 
-  # check if the string is equal to its reverse
   return string == reversed_string
 
 # test the function
-print(is_palindrome("racecar"))  # True
+print(is_palindrome("mama"))  # True
 print(is_palindrome("hello"))  # False
