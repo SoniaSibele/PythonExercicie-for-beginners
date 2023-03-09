9e1bea2462829e88ae4808e84eee2e8a3faeed03
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2023-03-09 21:09:30 +0000

    Update Exerc2_2.py

--- Exerc2_2.py
+++ Exerc2_2.py
@@ -1,3 +1,7 @@
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois 
+#modifique o programa para que ele mostre os números um ao lado do outro. Deve ser utilizado 
+#o ciclo do-while.
+
 i = 1
 while True:
     print(i)
