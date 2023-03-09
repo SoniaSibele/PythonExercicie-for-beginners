0aa107ad3993693332882e03fe62bf6d9d8b2180
SoniaRodrigues <76952338+SoniaSibele@users.noreply.github.com>
2023-03-09 21:22:08 +0000

    Update Exerc2_11.py

--- Exerc2_11.py
+++ Exerc2_11.py
@@ -1,3 +1,6 @@
+#Faça um programa que peça um numero inteiro positivo e em seguida mostre este numero 
+#invertido. 
+
 numero = input("Digite um inteiro positivo: ")
 numero = numero[::-1]
 print(f"Numero invertido: {numero}")
